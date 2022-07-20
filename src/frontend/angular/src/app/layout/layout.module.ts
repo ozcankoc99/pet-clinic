bf6423faf5f86b2eb34eff7dfed92802f5d33b8f
Ozcan KOC <ozcankoc99@gmail.com>
2022-07-20 16:22:29 +0300

    added sidebar

--- src/frontend/angular/src/app/layout/layout.module.ts
+++ src/frontend/angular/src/app/layout/layout.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { ShellLayoutComponent } from './shell-layout/shell-layout.component';
 import { NoLayoutComponent } from './no-layout/no-layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     ShellLayoutComponent,
-    NoLayoutComponent
+    NoLayoutComponent,
+    SidebarComponent,
+    TopMenuComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
