36ef1a27b066e00d73514bb8594e3de0cc3dae66
Ozcan KOC <ozcankoc99@gmail.com>
2022-07-22 22:59:45 +0300

    simplify

--- src/frontend/angular/src/app/test/simple-test/simple-test.component.ts
+++ src/frontend/angular/src/app/test/simple-test/simple-test.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-simple-test',
   templateUrl: './simple-test.component.html',
-  styleUrls: ['./simple-test.component.css'],
 })
-export class SimpleTestComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class SimpleTestComponent {}
